884901bfa473bbd1712ac54c2d0dc09b464bb598
NITHIN V KUMAR <R20200892.NITHINV@cse.reva.edu.in>
2025-10-19 09:53:16 +0530

    Added basic server Setup in Go

---
+++ AuthInGo/README.md
@@ -0,0 +1,2 @@
+install go get github.com/joho/godotenv this is to install dotenv library for loading environment variables from a .env file.
+install go get -u github.com/go-chi/chi/v5 this is to install the chi router for building HTTP services in Go.
---
+++ AuthInGo/app/application.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Config struct {
+	Addr string // PORT
+}
+
+type Application struct {
+	Config Config
+}
+
+func (app *Application) Run() error {
+	server := &http.Server{
+		Addr:         app.Config.Addr,
+		Handler:      nil,
+		ReadTimeout:  10 * time.Second, // Set read timeout	to 10 seconds
+		WriteTimeout: 10 * time.Second, // Set write timeout to 10 seconds
+	}
+	fmt.Printf("Starting server on %s\n", app.Config.Addr)
+	return server.ListenAndServe()
+}
